9a7e765522a8f8058b1ae5ebf46d75a60e9c90c5
Jonathan Gonzalez <jonathangon.2014@gmail.com>
2023-12-21 20:07:05 -0500

    Day 3. Finally

--- rust/src/day3.rs
+++ rust/src/day3.rs
@@ -59,30 +59,112 @@ fn part_1(lines: &[String]) -> u32 {
         }
     }
 
-    for i in &numbers {
-        println!("{i}");
-    }
-    // for line in &new_lines {
-    //     for letter in line {
-    //         print!("{letter}");
-    //     }
-    //     println!();
+    // for i in &numbers {
+    //     println!("{i}");
     // }
-
     numbers.iter().sum()
 }
 
+#[derive(Debug)]
+struct Gear {
+    x: usize,
+    y: usize,
+    numbers: Vec<u32>,
+}
+
+fn is_symbol(
+    lines: &Vec<Vec<char>>,
+    x: &isize,
+    y1: &usize,
+    y2: &usize,
+    number: &u32,
+    gears: &mut Vec<Gear>,
+) {
+    let h = lines.len();
+    let w = lines[0].len();
+
+    if *x >= 0_isize && *x < h as isize {
+        for i in (*y1 as isize - 1)..(*y2 as isize + 1) {
+            if i >= 0 && i < w as isize && lines[*x as usize][i as usize] == '*' {
+                if let Some(result) = gears
+                    .iter_mut()
+                    .find(|gear| gear.x == *x as usize && gear.y == i as usize)
+                {
+                    result.numbers.push(*number);
+                } else {
+                    gears.push(Gear {
+                        x: *x as usize,
+                        y: i as usize,
+                        numbers: vec![*number],
+                    });
+                }
+            }
+        }
+    }
+}
+
+fn part_2(lines: &[String]) -> u32 {
+    let mut gears = Vec::<Gear>::new();
+    let new_lines: Vec<Vec<char>> = lines
+        .iter()
+        .map(|line| line.chars().collect::<Vec<char>>())
+        .collect();
+    let width = new_lines[0].len();
+
+    for (x, line) in new_lines.iter().enumerate() {
+        let mut y = 0;
+        while y < width {
+            let mut number = 0;
+            let start = y;
+
+            while y < width && line[y].is_ascii_digit() {
+                let digit = line[y].to_digit(10).unwrap();
+                number = number * 10 + digit;
+                y += 1;
+            }
+
+            if number == 0 {
+                y += 1;
+                continue;
+            }
+
+            for idx in [-1, 0, 1].iter() {
+                is_symbol(
+                    &new_lines,
+                    &(x as isize + idx),
+                    &start,
+                    &y,
+                    &number,
+                    &mut gears,
+                );
+            }
+
+            y += 1;
+        }
+    }
+
+    let mut answer: u32 = 0;
+
+    for gear in &gears {
+        if gear.numbers.len() == 2 {
+            answer += gear.numbers.iter().product::<u32>();
+        }
+    }
+
+    answer
+}
+
 fn main() -> Result<()> {
-    if let Ok(file) = read_file("../day3.txt") {
+    if let Ok(file) = read_file("./src/input/day3.txt") {
         let answer = part_1(&file);
         println!("Answer: {}", answer);
     } else {
         eprintln!("ERROR: File not found");
     }
 
-    if let Ok(_file) = read_file("./src/input/day3.txt") {
-        // let answer = part_2(&file);
-        // println!("Answer: {}", answer);
+    if let Ok(file) = read_file("./src/input/day3.txt") {
+        let answer = part_2(&file);
+        println!("Answer: {}", answer);
     } else {
         eprintln!("ERROR: File not found");
     }
