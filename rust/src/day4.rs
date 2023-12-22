17a3ffa569fb7236111771ba463d9d49ea9bdb6f
Jonathan Gonzalez <jonathangon.2014@gmail.com>
2023-12-22 15:06:36 -0500

    Finshed Day 4 and Add new read file fn

--- rust/src/day4.rs
+++ rust/src/day4.rs
@@ -1,25 +1,97 @@
 use anyhow::Result;
-use aoc::read_file;
+// use aoc::read_file;
+use aoc::read_file_to_lines;
+use regex::Regex;
+use std::collections::HashSet;
 
-fn part_1(lines: &[String]) -> u32 {
-    let mut card_points = Vec::<u32>::new();
+fn part_1(lines: &Vec<Vec<char>>) -> u32 {
+    let re = Regex::new(r"\d+").unwrap();
+    let mut sum = 0;
 
-    for (row, line) in &lines.iter().enumerate() {}
+    for line in lines {
+        let mut my_num: HashSet<u32> = HashSet::new();
+        let mut i = 0;
 
-    card_points.iter().sum()
+        let line = line
+            .split(|letter| *letter == ':' || *letter == '|')
+            .collect::<Vec<_>>()
+            .iter()
+            .map(|slice| slice.iter().collect::<String>())
+            .collect::<Vec<String>>();
+
+        for m in re.find_iter(line[1].as_str()) {
+            let m = m.as_str().parse::<u32>().unwrap();
+            my_num.insert(m);
+        }
+
+        for win_num in re.find_iter(line[2].as_str()) {
+            let win_num = win_num.as_str().parse::<u32>().unwrap();
+            if my_num.contains(&win_num) {
+                i += 1;
+            }
+        }
+
+        if i > 0 {
+            sum += 2_u32.pow(i - 1);
+        }
+    }
+    sum
+}
+
+fn part_2(lines: &Vec<Vec<char>>) -> u32 {
+    let re = Regex::new(r"\d+").unwrap();
+    let mut instances: Vec<u32> = vec![1; lines.len()];
+
+    for (x, line) in lines.iter().enumerate() {
+        let mut my_num: HashSet<u32> = HashSet::new();
+        let mut i = 0;
+
+        let line = line
+            .split(|letter| *letter == ':' || *letter == '|')
+            .collect::<Vec<_>>()
+            .iter()
+            .map(|slice| slice.iter().collect::<String>())
+            .collect::<Vec<String>>();
+
+        for m in re.find_iter(line[1].as_str()) {
+            let m = m.as_str().parse::<u32>().unwrap();
+            my_num.insert(m);
+        }
+
+        for win_num in re.find_iter(line[2].as_str()) {
+            let win_num = win_num.as_str().parse::<u32>().unwrap();
+            if my_num.contains(&win_num) {
+                i += 1;
+            }
+        }
+        // 1 2 3 4 4 1
+
+        let mut j = 1;
+        while i > 0 {
+            if x > 0 {
+                instances[x + j] += instances[x];
+            } else {
+                instances[x + j] += 1;
+            }
+            j += 1;
+            i -= 1;
+        }
+    }
+
+    instances.iter().sum()
 }
 
 fn main() -> Result<()> {
-    if let Ok(file) = read_file("./src/input/day3.txt") {
+    if let Ok(file) = read_file_to_lines("./src/input/day4.txt") {
         let answer = part_1(&file);
         println!("Answer: {}", answer);
     } else {
         eprintln!("ERROR: File not found");
     }
 
-    if let Ok(file) = read_file("./src/input/day3.txt") {
-        // let answer = part_2(&file);
-        // println!("Answer: {}", answer);
+    if let Ok(file) = read_file_to_lines("./src/input/day4.txt") {
+        let answer = part_2(&file);
+        println!("Answer: {}", answer);
     } else {
         eprintln!("ERROR: File not found");
     }
