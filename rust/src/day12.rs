318d535b6d99cb27288066c8ff1cd11875d43ef5
Jonathan Gonzalez <jonathangon.2014@gmail.com>
2024-01-01 12:20:50 -0500

    Device1 -> Device2 Day 13

--- rust/src/day12.rs
+++ rust/src/day12.rs
@@ -1,58 +1,24 @@
 use anyhow::Result;
-use aoc::testing;
+use aoc::print_answers;
 use std::collections::HashMap;
 
 // TODO: Understand the algorithm
-// TODO: Break it down into part_1 and part_2
 
-fn n_arr<'a>(spring: &'a str, arr: &'a [usize]) -> usize {
-    let new_spring = spring.chars().collect::<Vec<char>>();
-
-    if arr.is_empty() {
-        if new_spring.contains(&'#') {
-            return 0;
-        }
-        return 1;
-    }
-
-    if new_spring.is_empty() {
-        if arr.is_empty() {
-            return 1;
-        }
-        return 0;
-    }
-
-    if new_spring.len() < arr.iter().sum::<usize>() + arr.len() - 1 {
-        return 0;
-    }
-
-    let mut result: usize = 0;
-    let n = arr[0];
-    let end: usize = (n + 1).min(spring.len());
-
-    if new_spring[0] == '.' || new_spring[0] == '?' {
-        result += n_arr(&spring[1..], arr);
-    }
-
-    if new_spring[0] == '#' || new_spring[0] == '?' {
-        if n <= spring.len()
-            && !spring[..n].contains('.')
-            && (n == spring.len() || new_spring[n] != '#')
-        {
-            result += n_arr(&spring[end..], &arr[1..]);
-        }
-    }
-
-    result
-}
-
-fn n_arr2<'a>(
+fn n_arr<'a>(
     cache: &mut HashMap<(&'a str, &'a [usize]), usize>,
     spring: &'a str,
     arr: &'a [usize],
 ) -> usize {
+    // Check if we've done the problem before
+    let key = (spring.clone(), arr.clone());
+    if let Some(&result) = cache.get(&key) {
+        return result;
+    }
+
     let new_spring = spring.chars().collect::<Vec<char>>();
 
+    // Check if the number of arrangments is empty and return 1 if there
+    // are not any extra '#' after our current position
     if arr.is_empty() {
         if new_spring.contains(&'#') {
             return 0;
@@ -60,6 +26,8 @@ fn n_arr2<'a>(
         return 1;
     }
 
+    // Checks if the string is empty and return 1 if the number of arrangements
+    // is also empty.
     if new_spring.is_empty() {
         if arr.is_empty() {
             return 1;
@@ -67,14 +35,10 @@ fn n_arr2<'a>(
         return 0;
     }
 
-    if new_spring.len() < arr.iter().sum::<usize>() + arr.len() - 1 {
+    // Checks the sum of the number of arrangements is greater than the length of
+    // the string if it is, then, return 0.
+    if new_spring.len() < arr.iter().sum::<usize>() {
         return 0;
-    }
-
-    let key = (spring.clone(), arr.clone());
-
-    if let Some(&result) = cache.get(&key) {
-        return result;
     }
 
     let mut result: usize = 0;
@@ -82,16 +46,15 @@ fn n_arr2<'a>(
     let end: usize = (n + 1).min(spring.len());
 
     if new_spring[0] == '.' || new_spring[0] == '?' {
-        result += n_arr2(cache, &spring[1..], arr);
+        result += n_arr(cache, &spring[1..], arr);
     }
 
-    if new_spring[0] == '#' || new_spring[0] == '?' {
-        if n <= spring.len()
-            && !spring[..n].contains('.')
-            && (n == spring.len() || new_spring[n] != '#')
-        {
-            result += n_arr2(cache, &spring[end..], &arr[1..]);
-        }
+    if (new_spring[0] == '#' || new_spring[0] == '?')
+        && n <= spring.len()
+        && !spring[..n].contains('.')
+        && (n == spring.len() || new_spring[n] != '#')
+    {
+        result += n_arr(cache, &spring[end..], &arr[1..]);
     }
 
     cache.insert(key, result);
@@ -100,6 +63,32 @@ fn n_arr2<'a>(
 }
 
 fn part_1(lines: &[String]) -> usize {
+    let mut damaged_springs: Vec<String> = Vec::new();
+    let mut arrangments: Vec<Vec<usize>> = Vec::new();
+    let mut sum: usize = 0;
+
+    for line in lines {
+        let line: Vec<&str> = line.split(' ').collect();
+        let ds = line[0].to_string();
+        let arr = line[1]
+            .split(',')
+            .map(|d| d.parse::<usize>().unwrap())
+            .collect::<Vec<usize>>();
+
+        damaged_springs.push(ds);
+        arrangments.push(arr);
+    }
+
+    let mut cache = HashMap::new();
+
+    for spring in 0..damaged_springs.len() {
+        let n = n_arr(&mut cache, &damaged_springs[spring], &arrangments[spring]);
+        sum += n;
+    }
+    sum
+}
+
+fn part_2(lines: &[String]) -> usize {
     let mut damaged_springs: Vec<String> = Vec::new();
     let mut arrangments: Vec<Vec<usize>> = Vec::new();
     let mut sum: usize = 0;
@@ -117,12 +106,6 @@ fn part_1(lines: &[String]) -> usize {
             .collect::<Vec<usize>>()
             .repeat(5);
 
-        // let ds = line[0].to_string();
-        // let arr = line[1]
-        //     .split(',')
-        //     .map(|d| d.parse::<usize>().unwrap())
-        //     .collect::<Vec<usize>>();
-        //
         damaged_springs.push(ds);
         arrangments.push(arr);
     }
@@ -130,13 +113,12 @@ fn part_1(lines: &[String]) -> usize {
     let mut cache = HashMap::new();
 
     for spring in 0..damaged_springs.len() {
-        let n = n_arr2(&mut cache, &damaged_springs[spring], &arrangments[spring]);
+        let n = n_arr(&mut cache, &damaged_springs[spring], &arrangments[spring]);
         sum += n;
     }
     sum
 }
-
 fn main() -> Result<()> {
-    testing(12, &part_1, 525152);
+    print_answers(12, &part_1, &part_2);
     Ok(())
 }
